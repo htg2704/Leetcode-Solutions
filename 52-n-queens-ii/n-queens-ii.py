1de98a3dda59abff55b9720e9e376f0dcdfe46b1
Harshwardhan Tripathi <49567662+htg2704@users.noreply.github.com>
2025-11-27 19:48:08 +0530

    Time: 45 ms (8.69%) | Memory: 18 MB (7.40%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        def isSafe(board, row, col):
+            r, c = row, col
+            while r>=0 and c>=0:
+                if(board[r][c]=='Q'):
+                    return False
+                r-=1
+                c-=1
+            r, c = row, col
+            while r>=0:
+                if(board[r][c]=='Q'):
+                    return False
+                r-=1
+            r, c = row, col
+            while r>=0 and c<n:
+                if(board[r][c]=='Q'):
+                    return False
+                r-=1
+                c+=1
+            return True
+
+        def fun(row, ans, board):
+            if(row==len(board)):
+                ans.append(["".join(r) for r in board])
+                return
+            for col in range(n):
+                if isSafe(board, row, col):
+                    board[row][col]='Q'
+                    fun(row+1, ans, board)
+                    board[row][col]='.'
+
+        ans = []
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        fun(0, ans, board)
+        return len(ans)
+        
