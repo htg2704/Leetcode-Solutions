16e7eb142bf8a2da8cb9cdbee93188d2abc1ddc3
Harshwardhan Tripathi <49567662+htg2704@users.noreply.github.com>
2026-09-04 19:29:46 +0530

    Time: 3 ms (77.34%) | Memory: 19.4 MB (9.85%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.py
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.py
@@ -3,11 +3,11 @@ class Solution:
         n = len(nums)
         maxa, mina = [-1]*n, [10**10]*n
         maxa[0]=nums[0]
+        mina[n-1]=nums[n-1]
         for i in range(1,n):
             maxa[i]=max(maxa[i-1], nums[i])
-        mina[n-1]=nums[n-1]
-        for i in range(n-2,-1,-1):
-            mina[i]=min(mina[i+1], nums[i])
+            mina[n-i-1]=min(mina[n-i], nums[n-i-1])
+            
         for i in range(n):
             if(maxa[i]-mina[i]<=k):
                 return i
